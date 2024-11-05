7b7d88a4afbb93899e4c1b951cd1cbf2cf514bbf
Steven A. Zaluk <stevezaluk@gmail.com>
2024-11-05 13:13:03 -0500

    Implemented basic functionality for starting and stoping the socket

--- main.go
+++ main.go
@@ -1,7 +1,16 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/stevezaluk/go-protocol-poc/src"
+)
 
 func main() {
 	fmt.Println("go protocol")
+
+	serv := src.Server{}
+	serv.Start()
+
+	serv.Stop()
 }
--- src/server.go
+++ src/server.go
@@ -1,1 +1,27 @@
 package src
+
+import (
+	"fmt"
+	"net"
+)
+
+type Server struct {
+	Sock *net.Listener
+}
+
+func (server *Server) Start() {
+	fmt.Println("[info] Attempting to start socket")
+	listener, err := net.Listen("tcp", "127.0.0.1:8000")
+	if err != nil {
+		fmt.Println("[error] Starting connection failed")
+		panic(err)
+	}
+
+	server.Sock = &listener
+}
+
+func (server *Server) Stop() {
+	fmt.Println("[info] Attempting to stop socket")
+	serv := *server.Sock
+	serv.Close()
+}
